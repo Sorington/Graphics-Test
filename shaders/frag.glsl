84faf31204389134b462a8a7c2d0b6bdb4b1bc38
sorington <dbalaghi10@gmail.com>
2013-10-14 19:47:23 +0200

    added house model and diffuse lighting working!

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -10,8 +10,9 @@ uniform sampler2D textureSampler;
 void main()
 {
     vec3 materialDiffuseColor = texture2D(textureSampler, UV).rgb;
-    float ang = clamp(dot(normal, directionalLight), 0, 1);
-    float lightPower = 1.0;
-    vec3 lightColor = vec3(0.6, 0.4, 0.4);
-    color = vec4(materialDiffuseColor*lightColor*lightPower*ang, 1.0);
+    vec3 materialAmbientColor = materialDiffuseColor*vec3(0.2,0.2,0.2);
+    float ang = clamp(dot(normal, directionalLight), 0.0, 1.0);
+    float lightPower = 2.0;
+    vec3 lightColor = vec3(0.8, 0.6, 0.4);
+    color = vec4(materialAmbientColor + materialDiffuseColor*lightColor*lightPower*ang, 1.0);
 }
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -16,22 +16,13 @@ void main()
     vec4 v = vec4(position, 1.0);
     gl_Position =  MVP * v;
 
-    // Position of the vertex, in worldspace : M * position
-    vec3 Position_worldspace = vec4(M * v).xyz;
-
-    // Vector that goes from the vertex to the camera, in camera space.
-    // In camera space, the camera is at the origin (0,0,0).
-    vec3 vertexPosition_cameraspace = vec4(V * M * v).xyz;
-    vec3 EyeDirection_cameraspace = vec3(10,10,15) - position;
-
     // Vector that goes from the vertex to the light, in camera space. M is ommited because it's identity.
-    vec3 LightPosition_cameraspace = vec4( V * vec4(vec3(1000, 1000, 1500),1)).xyz;
-    vec3 LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
+    vec3 LightDirection_cameraspace = vec4(V*vec4(vec3(-1, 1, 1),0)).xyz;
 
     // Normal of the the vertex, in camera space
-    vec3 Normal_cameraspace = vec4( V * M * vec4(normal_in,0)).xyz; // Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
+    vec3 Normal_cameraspace = vec4(V*M*vec4(normal_in,0)).xyz; // Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
 
     UV = UV_in;
-    normal = Normal_cameraspace;
-    directionalLight = LightDirection_cameraspace;
+    normal = normalize(Normal_cameraspace);
+    directionalLight = normalize(LightDirection_cameraspace);
 }
